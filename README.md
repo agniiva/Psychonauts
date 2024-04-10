02f1d575dc67b1c878cc45af875899c20cb281c8
Agniva <dev@boringmarketing.com>
2024-04-10 19:10:59 +0530

    Ver0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+## .Psychonaut (The World Framework) - made with Lumentis
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>.Psychonaut (The World Framework) - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/agniiva/psychonauts",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="psychonauts.in" target="_blank">
+              .Psychonauts (First Ones) - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content=".Psychonaut (The World Framework)" />
+          <meta property="og:description" content="We are talking to a culture, a society called .psychonaut (dot psychonaut) the dot was added because the concept is big, its about  a   smart culture that colives with nature, capitalism, and  can think, by utilising a new way to look at life, either by meditation or self awareness or by consuming plant medicines that help you  live a  fulfilling non rat life." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
